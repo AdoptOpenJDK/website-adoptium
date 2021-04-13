51f27b2de9fb6942700554c7d8906765f393104b
hiddenmind3 <hirschi.evan@gmail.com>
2021-04-13 10:28:16 +0200

    added primitive ua-parser

--- src/main/java/net/adoptium/IndexResource.java
+++ src/main/java/net/adoptium/IndexResource.java
@@ -16,25 +16,38 @@ public class IndexResource {
     Template index;
 
     @GET
-    @Produces(MediaType.TEXT_HTML)
-    public TemplateInstance get(@QueryParam("name") String name) {
-        return index.data("name", name);
-    }
-
-    @GET
     @Path("/useragent")
     @Produces(MediaType.TEXT_HTML)
     public Response testGettingUserSystemInformation(@HeaderParam("user-agent") String userAgent) {
         return Response.status(200)
                 .entity("userAgent : " + userAgent)
                 .build();
-
     }
 
     @GET
     @Produces(MediaType.TEXT_HTML)
     public TemplateInstance testGettingUserSystemInformation2(@HeaderParam("user-agent") String userAgent) {
-        return index.data("user-agent", userAgent);
+        String[] osArch = getOsAndArch(userAgent);
+        return index.data("userAgent", userAgent)
+                .data("os", osArch[0])
+                .data("arch", osArch[1]);
+
+    }
+
+    private String[] getOsAndArch(String userAgent){
+        userAgent = userAgent.toLowerCase();
+        String[] osArch = new String[2];
+        if(userAgent.contains("windows")){
+            osArch[0] = "windows";
+        } else if(userAgent.contains("linux")){
+            osArch[0] = "linux";
+        }
+        if(userAgent.contains("x64") || userAgent.contains("x86_64") || userAgent.contains("64")){
+            osArch[1] = "x64";
+        } else if(userAgent.contains("x86")){
+            osArch[1] = "x86";
+        }
+        return osArch;
     }
 }
 
