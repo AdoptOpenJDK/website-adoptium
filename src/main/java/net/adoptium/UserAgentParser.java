e366b4b36e2fe32706f17cd99f959e24a75efc66
hiddenmind3 <hirschi.evan@gmail.com>
2021-04-14 11:55:40 +0200

    added uap-java Library to help parse the os from the useragent

--- src/main/java/net/adoptium/UserAgentParser.java
+++ src/main/java/net/adoptium/UserAgentParser.java
@@ -1,14 +1,12 @@
 package net.adoptium;
 
+import ua_parser.Parser;
+
 public class UserAgentParser {
     public static String[] getOsAndArch(String userAgent){
-        userAgent = userAgent.toLowerCase();
         String[] osArch = new String[2];
-        if(userAgent.contains("windows")){
-            osArch[0] = "windows";
-        } else if(userAgent.contains("linux")){
-            osArch[0] = "linux";
-        }
+        Parser parser = new Parser();
+        osArch[0] = parser.parse(userAgent).os.family;
         if(userAgent.contains("x64") || userAgent.contains("x86_64") || userAgent.contains("64")){
             osArch[1] = "x64";
         } else if(userAgent.contains("x86")){
