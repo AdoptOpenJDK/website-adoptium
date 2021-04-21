d784d7a3fda0269ec01e53092a789aee818c668a
hiddenmind3 <hirschi.evan@gmail.com>
2021-04-21 20:48:07 +0200

    added test, which tests if there is a /docs/"file-name".html for each asciidoc in src/docs/asciidoc

--- src/test/java/net/adoptium/AsciiDocTest.java
+++ src/test/java/net/adoptium/AsciiDocTest.java
@@ -4,8 +4,12 @@ import com.microsoft.playwright.*;
 import io.quarkus.test.junit.QuarkusTest;
 import org.junit.jupiter.api.Test;
 
+import java.io.*;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Scanner;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 @QuarkusTest
 public class AsciiDocTest {
@@ -20,11 +24,19 @@ public class AsciiDocTest {
                     playwright.firefox()
             );
             for (BrowserType browserType : browserTypes) {
-                try (Browser browser = browserType.launch()) {
-                    BrowserContext context = browser.newContext();
-                    Page page = context.newPage();
-                    page.navigate("http://localhost:8081/docs/");
-                    System.out.println(page.url());
+                File dir = new File("src/docs/asciidoc");
+                Object[] filesObjectArray = Arrays.stream(dir.listFiles()).filter(p -> p.isFile() == true).toArray();
+                File[] filesInDir = Arrays.copyOf(filesObjectArray, filesObjectArray.length, File[].class);
+                for (File file : filesInDir) {
+                    try (Browser browser = browserType.launch()) {
+                        BrowserContext context = browser.newContext();
+                        Page page = context.newPage();
+                        try {
+                            page.navigate(("http://localhost:8081/docs/" + file.getName()).replaceFirst("[.][^.]+$", "") + ".html");
+                        } catch (PlaywrightException e) {
+                            fail("Failed because http://localhost:8081/docs/" + file.getName().replaceFirst("[.][^.]+$", "") + ".html does not exist", e);
+                        }
+                    }
                 }
             }
         }
