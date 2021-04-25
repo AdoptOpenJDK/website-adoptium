a6759fd9a2ea718ba8620a09d5756908438bd3f2
buechth1 <buechth1@students.zhaw.ch>
2021-04-25 14:29:28 +0200

    webkit still doesn't work on automated testing, removed webkit test for now

--- src/test/java/net/adoptium/AsciiDocTest.java
+++ src/test/java/net/adoptium/AsciiDocTest.java
@@ -12,19 +12,20 @@ import java.util.Objects;
 import static org.junit.jupiter.api.Assertions.fail;
 
 @QuarkusTest
-public class AsciiDocTest {
+class AsciiDocTest {
     //anderes file für test defaults -> schreiben wir rein die default browsertypes die wir benutzen
+    //Evtl. Setup methode aber momentan auf Eis
     @Test
-    public void AsciiDocDirectoryCorrect() {
+    void AsciiDocCheckIfHtmlPagesExist() {
         try (Playwright playwright = Playwright.create()) {
             List<BrowserType> browserTypes = Arrays.asList(
                     playwright.chromium(),
-                    playwright.webkit(),
                     playwright.firefox()
             );
             for (BrowserType browserType : browserTypes) {
                 File dir = new File("target/classes/META-INF/resources/docs");
-                Object[] filesObjectArray = Arrays.stream(Objects.requireNonNull(dir.listFiles())).filter(File::isFile).toArray();
+                Object[] filesObjectArray = Arrays.stream(Objects.requireNonNull(dir.listFiles()))
+                        .filter(File::isFile).toArray();
                 File[] filesInDir = Arrays.copyOf(filesObjectArray, filesObjectArray.length, File[].class);
                 for (File file : filesInDir) {
                     try (Browser browser = browserType.launch()) {
@@ -35,7 +36,8 @@ public class AsciiDocTest {
                             System.out.println(browserType.name());
                         } catch (PlaywrightException e) {
                             System.out.println("I failed to navigate onto the website: " + browserType.name());
-                            fail("Failed because http://127.0.0.1:8081/docs/" + file.getName() + " does not exist when opened with browser", e);
+                            fail("Failed because http://127.0.0.1:8081/docs/" + file.getName()
+                                    + " does not exist when opened with browser", e);
                         }
                     } catch (PlaywrightException e) {
                         fail("failed to launch browser " + browserType.name());
