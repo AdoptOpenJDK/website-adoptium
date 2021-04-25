fe775628ec4973db4af3a6e5d423ebc57ebca094
EnnioSileno <61289714+enniosileno@users.noreply.github.com>
2021-04-25 21:32:19 +0200

    First design of DownloadStringArgumentExtractor
    Signed-off-by: Ennio Sileno <silenenn@students.zhaw.ch>

---
+++ src/main/java/net/adoptium/utils/DownloadStringArgumentExtractor.java
@@ -0,0 +1,58 @@
+package net.adoptium.utils;
+
+import net.adoptium.exceptions.DownloadInvalidArgumentException;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import static net.adoptium.utils.DownloadArgumentGroup.*;
+
+public class DownloadStringArgumentExtractor {
+
+    /*
+    This Pattern is used to extract the arguments from the path parameter {args} in the GET-method from {DownloadResource}.
+    The pattern is not used to check if the arguments are valid. It checks if the string to match holds enough arguments
+    to build the download-api-request and puts the found arguments into their specified groups.
+
+    Groups:     Found in the enum {DownloadArgumentGroup}
+    Patterns:   [^-\/] accept anything except the -, \ and the / symbol
+                The - symbol outside of a group is used to distinguish between arguments and their groups.
+    Group example: (?<os>[^-\/]*) -> accept anything except the specified symbols and put the found substring in the group os
+    Group "version" is the only group that accepts - symbols.
+
+    Some examples for which the pattern matches:
+        os-arch-jvm_impl-image_type-heap_size-project-release_type-vendor-version
+        windows-x64-hotspot-jdk-normal-jdk-ga-adoptopenjdk-11.0.10+9
+    */
+    private static final String REGEX_DOWNLOAD = "^(?<" + OS + ">[^-\\/]*)-" +
+                                                "(?<" + ARCH + ">[^-\\/]*)-" +
+                                                "(?<" + JVM_IMPL + ">[^-\\/]*)-" +
+                                                "(?<" + IMAGE_TYPE + ">[^-\\/]*)-" +
+                                                "(?<" + HEAP_SIZE + ">[^-\\/]*)-" +
+                                                "(?<" + PROJECT + ">[^-\\/]*)-" +
+                                                "(?<" + RELEASE_TYPE + ">[^-\\/]*)-" +
+                                                "(?<" + VENDOR + ">[^-\\/]*)-" +
+                                                "(?<" + VERSION + ">[^\\/]*)$";
+
+    private static final Pattern DOWNLOAD_PATTERN = Pattern.compile(REGEX_DOWNLOAD, Pattern.CASE_INSENSITIVE);
+
+    public static Map<DownloadArgumentGroup, String> getVersionArguments(String stringVersionArguments) throws DownloadInvalidArgumentException {
+        Matcher matcher = DOWNLOAD_PATTERN.matcher(stringVersionArguments);
+        if(!matcher.find()) {
+            throw new DownloadInvalidArgumentException("Version not found!", "Try to access this page from the root route.");
+        }
+        Map<DownloadArgumentGroup, String> versionArguments = extractArgumentsToMap(matcher);
+        return versionArguments;
+    }
+
+    private static Map<DownloadArgumentGroup, String> extractArgumentsToMap(Matcher matcher) {
+        Map<DownloadArgumentGroup, String> versionArguments = new HashMap<>();
+        for(DownloadArgumentGroup downloadArgGroup : DownloadArgumentGroup.values()) {
+            String argument = matcher.group(downloadArgGroup.toString());
+            versionArguments.put(downloadArgGroup, argument);
+        }
+        return versionArguments;
+    }
+}
---
+++ src/test/java/net/adoptium/utils/DownloadStringArgumentExtractorTest.java
@@ -0,0 +1,60 @@
+package net.adoptium.utils;
+
+import net.adoptium.exceptions.DownloadInvalidArgumentException;
+import org.junit.jupiter.api.Test;
+
+import java.util.Map;
+
+import static net.adoptium.utils.DownloadArgumentGroup.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+class DownloadStringArgumentExtractorTest {
+
+    @Test
+    public void testArgumentGroups() {
+        String testString = "windows-x64-hotspot-jdk-normal-jdk-ga-adoptopenjdk-11.0.10+9";
+        Map<DownloadArgumentGroup, String> arguments = DownloadStringArgumentExtractor.getVersionArguments(testString);
+
+        assertEquals(DownloadArgumentGroup.values().length, arguments.size());
+
+        assertEquals("windows", arguments.get(OS));
+        assertEquals("x64", arguments.get(ARCH));
+        assertEquals("hotspot", arguments.get(JVM_IMPL));
+        assertEquals("jdk", arguments.get(IMAGE_TYPE));
+        assertEquals("normal", arguments.get(HEAP_SIZE));
+        assertEquals("jdk", arguments.get(PROJECT));
+        assertEquals("ga", arguments.get(RELEASE_TYPE));
+        assertEquals("adoptopenjdk", arguments.get(VENDOR));
+        assertEquals("11.0.10+9", arguments.get(VERSION));
+    }
+
+    @Test
+    public void testStringWithMinusSymbol() {
+        String testString = "windows-x64-hotspot-jdk-normal-jdk-ga-adoptopenjdk-11.0.10-9";
+        Map<DownloadArgumentGroup, String> arguments = DownloadStringArgumentExtractor.getVersionArguments(testString);
+
+        assertEquals(DownloadArgumentGroup.values().length, arguments.size());
+        assertEquals("11.0.10-9", arguments.get(VERSION));
+    }
+
+    @Test
+    public void testFindsGroupsWithEnoughArgumentsAlthoughNotValidArguments() {
+        //The DownloadStringArgumentExtractor should not check if the arguments are valid
+        String testString = "windows-x-64-hotspot-jdk-normal-jdk-ga-adoptopenjdk-11.0.10-9";
+        Map<DownloadArgumentGroup, String> arguments = DownloadStringArgumentExtractor.getVersionArguments(testString);
+
+        assertEquals(DownloadArgumentGroup.values().length, arguments.size());
+        assertEquals("x", arguments.get(ARCH));
+        assertEquals("adoptopenjdk-11.0.10-9", arguments.get(VERSION));
+    }
+
+    @Test
+    public void testInvalidStringPattern() {
+        //There are not enough - symbols to generate the needed arguments
+        String testString = "windows-x64-hotspot-jdk-normal-jdk-ga-adoptopenjdk";
+
+        assertThrows(DownloadInvalidArgumentException.class, () -> {
+           DownloadStringArgumentExtractor.getVersionArguments(testString);
+        });
+    }
+}
