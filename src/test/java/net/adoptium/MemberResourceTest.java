8080f6f4a2576ae0568de949141cb20ba7a2c76c
timhalter <tiimhalter@gmail.com>
2021-05-06 09:10:10 +0200

    Added unit test and test json files

--- src/test/java/net/adoptium/MemberResourceTest.java
+++ src/test/java/net/adoptium/MemberResourceTest.java
@@ -1,34 +1,77 @@
 package net.adoptium;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import io.quarkus.test.junit.QuarkusTest;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 
 import org.junit.Assert;
+
+import java.io.IOException;
+import java.net.URL;
 import java.util.List;
 
 @QuarkusTest
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class MemberResourceTest {
+    private MemberResource memberResource;
+    private URL validJsonURL, invalidJsonURL;
+    // Path of the testing json files
+    private final String VALIDJSON =  "json/members.json";
+    private final String INVALIDJSON = "src/test/java/net/adoptium/json/members_invalid.json";
 
+    @BeforeAll
+    public void init(){
+        memberResource = new MemberResource();
+        validJsonURL = memberResource.loadJSONURL(VALIDJSON);
+    }
 
-    @DisplayName("Test loading a JSON file")
+    @DisplayName("Test parsing a JSON file")
     @Test
     void parseJSONTest(){
-        String json = "{\"memberName\":\"Microsoft\",\"memberLink\":\"https://microsoft.com/\", \"memberLogo\":\"assets/memberLogos/microsoft.svg\", \"memberAltText\":\"Microsoft Logo\", \"organizationType\":\"\"}";
-        try {
-            MemberResource memberResource = new MemberResource();
-            List<Member> memberList = memberResource.getMemberList(json);
-            Member testMember = memberList.get(memberList.size() - 1);
-            Assert.assertEquals("Microsoft", testMember.getMemberName());
-            Assert.assertEquals("https://microsoft.com/", testMember.getMemberLink());
-            Assert.assertEquals("assets/memberLogos/microsoft.svg", testMember.getMemberLogo());
-            Assert.assertEquals("Microsoft Logo", testMember.getMemberAltText());
-            Assert.assertEquals("", testMember.getOrganizationType());
+        /**
+         * This test method will only test the deserialization of
+         * the first entry of the members.json file.
+         *
+         * To test a new entry, simply inject a new entry at
+         * the beginning of the members.json file and make sure
+         * that the injected info matches with the five Strings below.
+         * These five Strings must be initialized according to the new
+         * injected entry you want to test.
+         */
+        String memberName = "Microsoft";
+        String memberLink = "https://microsoft.com/";
+        String memberLogo = "assets/memberLogos/microsoft.svg";
+        String memberAltText = "Microsoft Logo";
+        String organizationType = "strategic";
 
-        } catch (Exception e) {
-            e.printStackTrace();
+        // Deserialize JSON file by URL
+        List<Member> memberList = memberResource.getMemberList(validJsonURL);
+        // Get the first Member
+        Member testMember = memberList.get(0);
+
+        Assert.assertEquals(memberName, testMember.getMemberName());
+        Assert.assertEquals(memberLink, testMember.getMemberLink());
+        Assert.assertEquals(memberLogo, testMember.getMemberLogo());
+        Assert.assertEquals(memberAltText, testMember.getMemberAltText());
+        Assert.assertEquals(organizationType, testMember.getOrganizationType());
+    }
+
+    @DisplayName("Test reading a valid JSON file")
+    @Test
+    void readValidJSON(){
+        Assert.assertTrue(isJSONValid(validJsonURL));
+    }
+
+    private boolean isJSONValid(URL jsonURL) {
+        try {
+            final ObjectMapper mapper = new ObjectMapper();
+            mapper.readTree(jsonURL);
+            return true;
+        } catch (IOException e) {
+            return false;
         }
     }
 }
