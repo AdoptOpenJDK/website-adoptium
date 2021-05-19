9db13e78c86827a7775e5b9ef8398918ab92eac3
hiddenmind3 <hirschi.evan@gmail.com>
2021-05-19 18:14:13 +0200

    created DocumentationResourceTest

---
+++ src/test/java/net/adoptium/DocumentationResourceTest.java
@@ -0,0 +1,36 @@
+package net.adoptium;
+
+import com.microsoft.playwright.*;
+import io.quarkus.test.junit.QuarkusTest;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@QuarkusTest
+public class DocumentationResourceTest {
+
+    private static final String existingDocPageName = "testdoc1";
+    private static final String nonexistantDocPageName = "thisPageDoesntExist";
+
+
+    @Test
+    void testGetExistingDoc() {
+        Playwright playwright = Playwright.create();
+
+        // TODO use global config
+        BrowserType browserType = playwright.firefox();
+
+        try (Browser browser = browserType.launch()) {
+            BrowserContext context = browser.newContext(new Browser.NewContextOptions().setLocale("en-US"));
+            Page page = context.newPage();
+
+            try {
+                page.navigate("localhost:8181/documentation" + existingDocPageName);
+            } catch (PlaywrightException e) {
+                fail("failed to navigate to page /documentation/" + existingDocPageName);
+            }
+        } catch (PlaywrightException e) {
+            fail("failed to launch browser " + browserType.name());
+        }
+    }
+}
