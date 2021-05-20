d540c9e6297dd07576d02f56530d184f10d986b0
Julius Berger <julius.berger@protonmail.ch>
2021-05-20 14:32:42 +0200

    i18n exceptions improved, though we're still waiting on quarkus issue #12792

--- src/main/java/net/adoptium/model/DownloadErrorTemplate.java
+++ src/main/java/net/adoptium/model/DownloadErrorTemplate.java
@@ -1,18 +1,12 @@
 package net.adoptium.model;
 
-import io.quarkus.qute.Engine;
-
 public class DownloadErrorTemplate {
     private final String errorMessage;
     private final String suggestion;
 
-    public DownloadErrorTemplate(Engine engine, String errorMessageID, String suggestionID) {
-        this.errorMessage = getI18N(engine, errorMessageID);
-        this.suggestion = getI18N(engine, suggestionID);
-    }
-
-    private String getI18N(Engine engine, String id) {
-        return engine.parse("{msg:" + id + "}").render();
+    public DownloadErrorTemplate(String errorMessage, String suggestion) {
+        this.errorMessage = errorMessage;
+        this.suggestion = suggestion;
     }
 
     public String getErrorMessage() {
